f16f0efa807f1cf8ec2f4eb4a4131d4bd544ea6e
xtreemtech <108930783+xtreemtech@users.noreply.github.com>
2022-11-04 00:49:04 +0100

    Update _puts.c

--- _puts.c
+++ _puts.c
@@ -1,1 +1,16 @@
-C - Simple Shell
+#include "holberton.h"
+/**
+ * _puts - prints a string
+ * @str: pointer to the string to print
+ * Return: void
+*/
+
+void _puts(char *str)
+{
+int i = 0;
+while (str[i])
+{
+	_putchar(str[i]);
+	i++;
+}
+}
