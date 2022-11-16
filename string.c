ac81a8405394690feb7e520630d800e06125e2e1
Ostentive <104060490+Ostentive@users.noreply.github.com>
2022-11-16 20:13:37 +0100

    Update string.c

--- string.c
+++ string.c
@@ -112,3 +112,9 @@ void _puts(char *str)
 		i++;
 	}
 }
+
+/**
+* checkbuild - checks if the command is a buildin
+* @arv: array of arguments
+* Return: pointer to function that takes arv and returns void
+*/
